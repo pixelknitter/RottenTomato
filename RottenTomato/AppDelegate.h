91aacb1673a787f37a3d41d282a8148a2e5e79ae
Eddie Freeman <eddie@ninjasudo.com>
2014-06-09 09:56:44 -0700

    initial commit

---
+++ RottenTomato/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  RottenTomato
+//
+//  Created by Eddie Freeman on 6/9/14.
+//  Copyright (c) 2014 NinjaSudo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
